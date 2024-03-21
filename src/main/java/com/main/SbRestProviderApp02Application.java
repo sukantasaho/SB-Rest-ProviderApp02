1ded3a09dddc5c9a58190be5037f12c2aaabb057
sukantasaho <ssukanta520@gmail.com>
2024-03-21 12:03:19 +0530

    commit

---
+++ src/main/java/com/main/SbRestProviderApp02Application.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestProviderApp02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestProviderApp02Application.class, args);
+	}
+
+}
